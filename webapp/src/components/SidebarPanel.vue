005f91cd7c3338fb94009f731b24329bc30b3649
golub <golubev1400@mail.ru>
2023-02-12 18:56:19 +0300

    some webapp fixes

--- webapp/src/components/SidebarPanel.vue
+++ webapp/src/components/SidebarPanel.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
-  <b-button v-b-toggle href="#example-sidebar" @click.prevent>Сотрудники</b-button>
-  <b-sidebar id="example-sidebar" title="Sidebar" shadow>
+  <b-button v-b-toggle href="#sidebar" @click.prevent>Сотрудники</b-button>
+  <b-sidebar id="sidebar"  :no-close-on-route-change="true" title="Sidebar" shadow>
     <div class="p-0">
       <left-panel v-for="o in object" :profile="o" :key="o.id"/>
     </div>
